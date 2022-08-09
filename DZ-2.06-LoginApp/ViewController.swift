d683b4bb30ee5c7c52d62965f62c5ec28b734e10
genas86 <sgm86@bk.ru>
2022-08-09 14:30:35 +0300

    Initial Commit

---
+++ DZ-2.06-LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ-2.06-LoginApp
+//
+//  Created by Genas1986 on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
